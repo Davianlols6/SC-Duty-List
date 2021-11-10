b315d3a7e890ab8622e535cd5aff2cf9e44854db
Davianlols6 <Davianenghongrui@gmail.com>
2021-11-10 17:12:19 +0800

    Update index.js

--- Website/index.js
+++ Website/index.js
@@ -161,7 +161,7 @@ $.getJSON("Data/data.json?time=" + new Date().getTime(),
         var dt = new Date();
         const monthFixer = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
 
-        var date = String(dt.getFullYear()) + "-" + String(monthFixer[dt.getMonth()]) + "-" + String(dt.getDate());
+        //var date = String(dt.getFullYear()) + "-" + String(monthFixer[dt.getMonth()]) + "-" + String(dt.getDate());
         $("#start").attr("min", Object.keys(data["dates"])[0]);
         $("#start").attr("max", Object.keys(data["dates"])[Object.keys(data["dates"]).length - 1]);
 
