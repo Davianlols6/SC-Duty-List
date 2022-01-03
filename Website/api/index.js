9c1b3df054f147aadc88bba5b2a5b09be56bfea4
Davianlols6 <Davianenghongrui@gmail.com>
2022-01-03 22:51:03 +0800

    Update

--- Website/api/index.js
+++ Website/api/index.js
@@ -13,6 +13,174 @@ app.get("/", (req, res) => {
   res.send("Hello World!!!");
 });
 
+app.get("/o/:day/:class", (req, res) => {
+  const { data1 } = require("./odata.json");
+  var jsonPath1 = path.join(__dirname, "..", "logs/olog.json");
+  const queryObject = url.parse(req.url,true).query;
+  const event = new Date();
+
+  fs.readFile(jsonPath1, 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+    obj.data.push({"time": event.toISOString(), "day": req.params.day, "class": req.params.class, "userAgent": req.headers['user-agent'], "uuid": queryObject.uuid}); //add some data
+    json = JSON.stringify(obj); //convert it back to json
+    fs.writeFile(jsonPath1, json, 'utf8', err => {
+      if (err) {
+       console.error('Failed to write starter kits file: ', err);
+      } else console.log('Fetched 1 file: ');
+     }); // write it back 
+  }});
+  res.send(
+    JSON.stringify({
+      data: data1[req.params.day][req.params.class]
+    })
+  );
+});
+
+app.get("/ologdata", (req, res) => {
+  const request = require('request');
+
+let url = "https://www.davianeng.com/sc_duty_list/logs/olog.json";
+
+let options = {json: true};
+
+// Goal of this code is to get 1) Total number of unique visitors all time (excluding null) 2) Total number of unique visitors per day (excluding null) 3) Total number of requests made per day
+
+let tempObject = {};
+
+let finalObject = {};
+
+request(url, options, (error, res, body) => {
+    if (error) {
+        return  console.log(error)
+    };
+
+    if (!error && res.statusCode == 200) {
+        
+        let tempArray1 = [];
+        let tempObject2 = {};
+        let tempObject3 = {};
+        let minObject1 = {};
+        let minObject2 = {};
+       body.data.forEach( (v, i) => {
+           if (v.uuid !== 'null') {
+            tempArray1.push(v.uuid);
+            let date = `${new Date(v.time).getDate()}-${new Date(v.time).getUTCMonth()+1}-${new Date(v.time).getFullYear()}`;
+            let minDate = `${new Date(v.time).getHours()}:00 ${new Date(v.time).getDate()}-${new Date(v.time).getUTCMonth()+1}-${new Date(v.time).getFullYear()}`;
+            if (tempObject2[date] === undefined) {
+                tempObject2[date] = [];
+            }
+            if (minObject1[minDate] === undefined) {
+                minObject1[minDate] = [];
+            }
+
+            tempObject2[date].push(v.uuid);
+            minObject1[minDate].push(v.uuid);
+            
+           }
+
+           let date = `${new Date(v.time).getDate()}-${new Date(v.time).getUTCMonth()+1}-${new Date(v.time).getFullYear()}`;
+            if (tempObject3[date] === undefined) {
+                tempObject3[date] = 0;
+            }
+
+            tempObject3[date] += 1;
+
+            let minDate = `${new Date(v.time).getHours()}:00 ${new Date(v.time).getDate()}-${new Date(v.time).getUTCMonth()+1}-${new Date(v.time).getFullYear()}`;
+            if (minObject2[minDate] === undefined) {
+                minObject2[minDate] = 0;
+            }
+
+            minObject2[minDate] += 1;
+
+       })
+
+       Date.prototype.addHours = function(h) {
+        this.setTime(this.getTime() + (h*60*60*1000));
+        return this;
+      }
+
+      Date.prototype.subHours = function(h) {
+        this.setTime(this.getTime() - (h*60*60*1000));
+        return this;
+      }
+
+       finalObject['Unique Visitors All Time'] = new Set(tempArray1).size;
+
+       for (i in tempObject2) {
+           if (finalObject['Unique Visitors Per Day'] === undefined) {
+            finalObject['Unique Visitors Per Day'] = {};
+           }
+
+           finalObject['Unique Visitors Per Day'][String(i)] = parseInt(new Set(tempObject2[i]).size);
+       }
+
+       let uniqueVisitorObj = {};
+       for (i in minObject1) {
+        if (finalObject['Unique Visitors Per Hour'] === undefined) {
+         finalObject['Unique Visitors Per Hour'] = {};
+        }
+
+     uniqueVisitorObj[String(i)] = parseInt(new Set(minObject1[i]).size);
+    }
+
+    let tempArray5 = [];
+       for (i in uniqueVisitorObj) {
+           tempArray5.push(new Date(`${i.split(' ')[1].split('-')[2]}-${i.split(' ')[1].split('-')[1]}-${i.split(' ')[1].split('-')[0]} ${i.split(' ')[0]}:00`))
+       }
+
+       let counter4 = tempArray5[0].subHours(1);
+       while (String(counter4) !== String(tempArray5[tempArray5.length-1])) {
+        let time = new Date(counter4).addHours(1);
+           let minDate = `${new Date(time).getHours()}:00 ${new Date(time).getDate()}-${new Date(time).getUTCMonth()+1}-${new Date(time).getFullYear()}`;
+           if (uniqueVisitorObj[minDate] === undefined) {
+            finalObject['Unique Visitors Per Hour'][minDate] = 0;
+           } else {
+            finalObject['Unique Visitors Per Hour'][minDate] = uniqueVisitorObj[minDate];
+           }
+           counter4 = time;
+       }
+
+       finalObject['Total Requests Per Day'] = tempObject3;
+
+       let counter1 = 0;
+       let counter2 = 0;
+       for (i in finalObject['Total Requests Per Day']) {
+        counter1 += finalObject['Total Requests Per Day'][i];
+       }
+       
+       finalObject['Total Requests All Time'] = counter1;
+       finalObject['Total Requests Per Hour'] = {};
+
+
+      let tempArray2 = [];
+       for (i in minObject2) {
+           tempArray2.push(new Date(`${i.split(' ')[1].split('-')[2]}-${i.split(' ')[1].split('-')[1]}-${i.split(' ')[1].split('-')[0]} ${i.split(' ')[0]}:00`))
+       }
+
+       let counter3 = tempArray2[0].subHours(1);
+       while (String(counter3) !== String(tempArray2[tempArray2.length-1])) {
+        let time = new Date(counter3).addHours(1);
+           let minDate = `${new Date(time).getHours()}:00 ${new Date(time).getDate()}-${new Date(time).getUTCMonth()+1}-${new Date(time).getFullYear()}`;
+           if (minObject2[minDate] === undefined) {
+            finalObject['Total Requests Per Hour'][minDate] = 0;
+           } else {
+            finalObject['Total Requests Per Hour'][minDate] = minObject2[minDate];
+           }
+           counter3 = time;
+       }
+      send();
+    };
+});
+
+function send() {
+  res.send(JSON.stringify(finalObject));
+}
+
+});
+
 app.get("/version", (req, res) => {
   var jsonPath = path.join(__dirname, "..", "Data", "data.json");
   let rawdata = fs.readFileSync(jsonPath);
@@ -20,7 +188,7 @@ app.get("/version", (req, res) => {
   const queryObject = url.parse(req.url,true).query;
   const event = new Date();
 
-  var jsonPath1 = path.join(__dirname, "..", "log.json");
+  var jsonPath1 = path.join(__dirname, "..", "logs/log.json");
 
 fs.readFile(jsonPath1, 'utf8', function readFileCallback(err, data){
   if (err){
